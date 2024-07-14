24609ab3a3505a71a69e00c385d13217412c4178
Andrea Prestia <andrea.prestia-ext@alstomgroup.com>
2024-07-14 22:09:54 +0200

    feat: add Timeseries index implementation

--- SpaceDb.Core/Rectangle.cs
+++ SpaceDb.Core/Rectangle.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SpaceDb.Core;
+﻿namespace SpaceDb.Core;
 
 public struct Rectangle
 {
